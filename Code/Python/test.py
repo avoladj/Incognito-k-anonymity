35a888d0cc6189133737699ef267e595870050cf
avoladj <4342472@studenti.unige.it>
2020-03-24 18:25:02 +0100

    inizio presentazione

--- Code/Python/test.py
+++ Code/Python/test.py
@@ -7,13 +7,14 @@ def execute(k):
     for i in range(2, 10):
         i_str = str(i)
         start = datetime.timestamp(datetime.now())
-        os.system("python3 main.py -d adult -D dimension_tables_" + i_str + "qi.json -k " + k_str)
+        os.system("python3 main.py -d ../datasets/test_adults.csv -D dimension_tables_" + i_str + "qi.json -k " + k_str)
         stop = datetime.timestamp(datetime.now())
         print()
         print("Execution time with " + i_str + " QIs and k= " + k_str + ": " + str(stop - start))
         print()
-        print("Waiting for 3 seconds...\n")
-        time.sleep(3)
+        yes = input("Continue? y/n")
+        if not (yes == "y" or yes == "Y"):
+            break
 
 
 if __name__ == "__main__":
